3d82ab27f78afc1f71f40da90264c74008441a7c
unknown <jcarreno@payall.com.ve>
2025-03-27 17:01:57 -0400

    Crear clase Media para serializar el json

---
+++ lib/model/Media.dart
@@ -0,0 +1,24 @@
+class Media{
+  late int id;
+  late double voteAverage;
+  late String title;
+  late String posterPath;
+  late String backdropPath;
+  late String overview;
+  late String releaseDate;
+  List<dynamic> genreId;
+  factory Media(Map jsonMap){
+
+  }
+  Media.deserialize(jsonMap):
+      id = jsonMap['id'].toInt(),
+      voteAverage = jsonMap['vote_average'].toDouble(),
+      title = jsonMap['title'],
+      posterPath = jsonMap['poster_path']??"",
+      backdropPath = jsonMap['backdrop_path']??"",
+      overview = jsonMap['overview'],
+      releaseDate = jsonMap['release_date'],
+      genreId = jsonMap['genre_ids'].toList();
+
+
+}
