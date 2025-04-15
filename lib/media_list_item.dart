d5a981d0a781934767555528f0eb65ce5d34ca42
jcarreno <jcarreno@payall.com.ve>
2025-04-15 17:18:32 -0400

    Colocar la valoracion de votos en la franja

--- lib/media_list_item.dart
+++ lib/media_list_item.dart
@@ -13,55 +13,77 @@ class MediaListItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Container(
-              child: Stack(children: <Widget>[
-            FadeInImage.assetNetwork(
-              placeholder: "assets/placeholder.jpg",
-              image: media.getBackDropUrl(),
-              fit: BoxFit.cover,
-              fadeInDuration: new Duration(milliseconds: 40),
-              height: 200.0,
-              width: double.infinity,
-            ),
-            Positioned(
-              left: 0.0,
-              bottom: 0.0,
-              right: 0.0,
-              child: new Container(
-                decoration: new BoxDecoration(
-                  color: Colors.grey[900]?.withOpacity(0.5),
+            child: Stack(
+              children: <Widget>[
+                FadeInImage.assetNetwork(
+                  placeholder: "assets/placeholder.jpg",
+                  image: media.getBackDropUrl(),
+                  fit: BoxFit.cover,
+                  fadeInDuration: new Duration(milliseconds: 40),
+                  height: 200.0,
+                  width: double.infinity,
                 ),
-                constraints: new BoxConstraints.expand(
-                  height: 55.0,
+                Positioned(
+                  left: 0.0,
+                  bottom: 0.0,
+                  right: 0.0,
+                  child: new Container(
+                    decoration: new BoxDecoration(
+                      color: Colors.grey[900]?.withOpacity(0.5),
+                    ),
+                    constraints: new BoxConstraints.expand(
+                      height: 55.0,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Positioned(
-                left: 10.0,
-                bottom: 30.0,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new Container(
-                      child: new Text(
-                        media.title,
-                        style: new TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                Positioned(
+                  left: 10.0,
+                  bottom: 30.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Container(
+                        child: new Text(
+                          media.title,
+                          style: new TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                    new Container(
-                      padding: const EdgeInsets.only(top:4.0),
-                      child: new Text(media.getGenres(),
-                      style: new TextStyle(color: Colors.white),
-                      maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-
-                    )
-                  ],
-                ))
-          ]))
+                      new Container(
+                        width: 250.0,
+                        padding: const EdgeInsets.only(top: 4.0),
+                        child: new Text(
+                          media.getGenres(),
+                          style: new TextStyle(color: Colors.white),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                new Positioned(
+                  right: 5.0,
+                  bottom: 10.0,
+                  child: new Column(
+                    children: <Widget>[
+                      new Row(
+                        children: <Widget>[
+                          new Text(media.voteAverage.toString()),
+                          new Container(
+                            width: 4.0,
+                          ),
+                          new Icon(Icons.star, color: Colors.white,size: 16.0,)
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
