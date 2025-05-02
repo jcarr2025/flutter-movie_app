b1beb3cbb9e9e8b8097bf2353f38eed1072910de
jcarreno <jcarreno@payall.com.ve>
2025-05-02 09:45:34 -0400

    aplicar desenfoque guassiano  a la aimagen de fondo

--- lib/media_detail.dart
+++ lib/media_detail.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:s15v189movie_app/model/Media.dart';
+import 'dart:ui' as ui;
+
 class MediaDetail extends StatelessWidget {
   final Media media;
+
   MediaDetail(this.media);
 
   @override
   Widget build(BuildContext context) {
-    return  new Scaffold(
+    return new Scaffold(
       body: new Stack(
         fit: StackFit.expand,
         children: [
           new Image.network(
             media.getBackDropUrl(),
             fit: BoxFit.cover,
-
-          )
+          ),
+          new BackdropFilter(
+            filter: new ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.9),
+            child: new Container(
+              color: Colors.black.withOpacity(0.5),
+            ),
+          ),
         ],
       ),
     );
