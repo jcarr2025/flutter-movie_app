4fbb5302cf1519fe3e4286afd0ba66cd9efa632e
jcarreno <jcarreno@payall.com.ve>
2025-05-08 16:33:58 -0400

    creacion del metodo para ejecutar el query

--- lib/resources/db_provider.dart
+++ lib/resources/db_provider.dart
@@ -3,23 +3,38 @@ import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'dart:io';
 
-class DBProvider{
+class DBProvider {
   Database? db;
 
-  DBProvider(){
+  DBProvider() {
     init();
   }
-   void init() async{
+
+  void init() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
 
     final path = join(documentsDirectory.path, "Cats4.db");
-    db = await openDatabase(path, version: 1, onCreate: (Database db, int version) async {
-      await db.execute("CREATE TABLE Cats ("
-          "id INTEGER PRIMARY KEY,"
-          "name TEXT,"
-          "age INTEGER,"
-          "image TEXT"
-          ")");
+    db = await openDatabase(
+        path, version: 1, onCreate: (Database newDb, int version) async {
+      await newDb.execute("""
+          CREATE TABLE Cats (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          profile_path TEXT,
+          movie_id INTEGER
+          )
+      """);
     });
-   }
+  }
+
+  fecthCats(int mediaId) async {
+    var maps = db?.query("Cast",
+        columns: null,
+        where: "movie_id = ?",
+        whereArgs: [mediaId]);
+    if (maps != null) {
+      return [];
+    }
+
+  }
 }
